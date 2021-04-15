df347e279b359aa6ffc05be8f0d24380ceb51b86
brunompasini <brunompasini@gmail.com>
2021-04-15 16:41:45 -0300

    react app

--- oldstuff/README.md
+++
@@ -1,2 +0,0 @@
-# brunompasini.github.io
-First iteration of my website, I'm really sad I got this idea after the college term started, prob gonna be unfinished for some time.
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/profilepic.png" className="App-logo" alt="logo" />
+        <p>
+          Website in the making, hope to make learn react along the way
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/brunompasini"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click here to see my Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
